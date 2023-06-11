9e0ad66b357662e43f4f52390280593b8bff4d5d
Mridul Roy <midlry.mr@gmail.com>
2023-06-11 07:07:21 +0530

    update

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -11,23 +11,25 @@ export function getSortedPostsData() {
     const fileNames = fs.readdirSync(postsDirectory)
     const allPostsData = fileNames.map(fileName => {
         // Remove ".md" from file name to get id
-        const id = fileName.replace(/\.md$/, '')
-
+        const id = fileName.replace(/\.mdx$/, '')
+        
         // Read markdown file as string
         const fullPath = path.join(postsDirectory, fileName)
         const fileContents = fs.readFileSync(fullPath, 'utf8')
-
+        
         // Use gray-matter to parse the post metadata section
         const matterResult = matter(fileContents)
-
+        
         // Combine the data with the id
         return {
             id,
+            Date,
             ...matterResult.data
         }
     })
     // Sort posts by date
     return allPostsData.sort((a, b) => {
+        console.log(a.Date)
         if (a.date < b.date) {
             return 1
         } else {
@@ -41,14 +43,14 @@ export function getAllPostIds() {
     return fileNames.map(fileName => {
         return {
             params: {
-                id: fileName.replace(/\.md$/, '')
+                id: fileName.replace(/\.mdx$/, '')
             }
         }
     })
 }
 
 export async function getPostData(id) {
-    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fullPath = path.join(postsDirectory, `${id}.mdx`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
   
     // Use gray-matter to parse the post metadata section
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -2,48 +2,6 @@ import { useTheme as useNextTheme } from 'next-themes'
 import { Grid, Switch, useTheme, Navbar, Image, Card, Spacer, Container, Text } from "@nextui-org/react";
 import { MoonIcon } from "./MoonIcon";
 import { SunIcon } from "./SunIcon";
-import styled from 'styled-components';
-const StyledHeroSection = styled.section`
-  ${({ theme }) => theme.mixins.flexCenter};
-  flex-direction: column;
-  align-items: flex-start;
-  min-height: 100vh;
-  height: 100vh;
-  padding: 0;
-
-  @media (max-height: 700px) and (min-width: 700px), (max-width: 360px) {
-    height: auto;
-    padding-top: var(--nav-height);
-  }
-
-  h1 {
-    margin: 0 0 30px 4px;
-    color: var(--green);
-    font-family: var(--font-mono);
-    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
-    font-weight: 400;
-
-    @media (max-width: 480px) {
-      margin: 0 0 20px 2px;
-    }
-  }
-
-  h3 {
-    margin-top: 5px;
-    color: var(--slate);
-    line-height: 0.9;
-  }
-
-  p {
-    margin: 20px 0 0;
-    max-width: 540px;
-  }
-
-  .email-link {
-    ${({ theme }) => theme.mixins.bigButton};
-    margin-top: 50px;
-  }
-`;
 
 export default function Test() {
   const { setTheme } = useNextTheme();
