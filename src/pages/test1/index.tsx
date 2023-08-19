a6dfcebcdcceaeccd04c5fc3c592cac788db830d
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2023-08-19 03:40:17 +0000

    Update

--- src/pages/test1/index.tsx
+++ src/pages/test1/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
  
 export default function IndexPage(props) {
   return (
-    <Link href="t.sh" locale="fr">
+    <Link href="test1/t.sh">
       t.sh
     </Link>
   )
