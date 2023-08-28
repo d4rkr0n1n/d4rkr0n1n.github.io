a23bfa3161ec428a831103c91a4c59edd91ce9ed
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2023-08-28 22:12:11 +0530

    Update user_data.sh

--- public/scripts/install/user_data.sh
+++ public/scripts/install/user_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 touch /home/ubuntu/test
 sudo apt-add-repository ppa:ansible/ansible -y
+echo "repo" >> /home/ubuntu/test
 sudo apt update -y
+echo "update" >> /home/ubuntu/test
 sudo apt install ansible -y
+echo "ansible" >> /home/ubuntu/test
